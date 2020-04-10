88bc704ac5391b4d85e2aeed1467f9078a8a5175
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-10 11:45:19 -0400

    PUT project by ID endpoint

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -8,4 +8,14 @@ router.get("/", (req, res) => {
     .then(projects => res.status(200).json(projects))
     .catch(err => res.status(500).json({ message: "Error getting all projects."}))});
 
+// PUT project by ID
+router.put("/:id", (req, res) => {
+    if(!req.body.name || !req.body.description) {
+        res.status(400).json({ error: "Error: project name or description missing!"})
+    };
+    Projects.update(req.params.id, req.body)
+    .then(project => project ? res.status(200).json(project) : res.status(404).json({ error: "Error: cannot find this project."}))
+    .catch(() => res.status(500).json({ error: "Error: unable to edit this project."}));
+});
+
 module.exports = router;
